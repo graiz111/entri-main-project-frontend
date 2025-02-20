be2d34fed47b5ae53b25751b15245a72d98a066e
Grace <grace@Graces-MacBook-Pro.local>
2025-02-20 12:20:36 +0530

    axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import Routess from "./routes/Routess";
 import { ThemeProvider } from "./context/ThemeContext";
-import { CartProvider } from './context/CartContext';
+
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <ThemeProvider>
-      <CartProvider>
+     
 
         <Routess />
         
-      </CartProvider>
+
   
     </ThemeProvider>
   );
